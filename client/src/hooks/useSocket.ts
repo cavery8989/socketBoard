ded5ba3838f45fbd78f7c4ac122f915baccdd329
cavery8989 <c.avery1989@gmail.com>
2020-11-26 20:41:34 +0000

    added more heroku things

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,9 +1,11 @@
-import {io} from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const endpoint = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'prodOne'
-const socket = io('http://localhost:3000')
+const endpoint =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://glacial-atoll-21218.herokuapp.com/ ";
 
-
+const socket = io(endpoint);
 export const useSocket = () => {
-    return {socket}
-}
+  return { socket };
+};
