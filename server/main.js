8330fc759c56b7c3ad95b138b82e414169e1ed9a
ChrisAveryCin <chris.avery@cinch.co.uk>
2020-11-26 14:05:43 +0000

    working example

---
+++ server/main.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cors = require('cors')
+var app = express(cors())
+var bindEvents = require('./socket/bindEvents')
+
+var http = require('http').createServer(app);
+
+
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "http://localhost:3001",
+      methods: ["GET", "POST"]
+    }
+  });
+
+io.on('connection', (socket) => {
+    console.log('there was a connection')
+    bindEvents(socket)
+   
+})
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
